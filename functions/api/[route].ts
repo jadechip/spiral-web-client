3d8c450ceccd1eac41d2d37ebfb6efd39b271e92
Jim Eric Skogman <jadechip@Jims-MacBook-Pro.local>
2024-07-20 10:25:29 +0700

    Revert to simple proxy function

--- functions/api/[route].ts
+++ functions/api/[route].ts
@@ -3,62 +3,11 @@
 // export const onRequest = handle(app)
 
 export async function onRequest(context) {
-  try {
-    const url = new URL(context.request.url);
-    url.hostname = "spiral-backend.jadechip.workers.dev";
-    url.protocol = "https";
+  const url = new URL(context.request.url);
+  url.hostname = "spiral-backend.jadechip.workers.dev";
 
-    // Log the original request URL
-    console.log("Original Request URL:", context.request.url);
+  const request = new Request(url.toString(), context.request);
+  const response = await fetch(request);
 
-    // Log the modified request URL
-    console.log("Modified Request URL:", url.toString());
-
-    const headers = new Headers(context.request.headers);
-    headers.set("Origin", "https://spiral-web-client.pages.dev");
-
-    const request = new Request(url.toString(), {
-      method: context.request.method,
-      headers: headers,
-      body:
-        context.request.method !== "GET" && context.request.method !== "HEAD"
-          ? context.request.body
-          : null,
-      credentials: "include",
-    });
-
-    // Log the headers
-    console.log("Request Headers:", [...headers.entries()]);
-
-    const response = await fetch(request);
-
-    const contentType = response.headers.get("content-type") || "";
-
-    if (contentType.includes("application/json")) {
-      const jsonResponse = await response.json();
-      return new Response(JSON.stringify(jsonResponse), {
-        status: response.status,
-        statusText: response.statusText,
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "https://spiral-web-client.pages.dev",
-          "Access-Control-Allow-Credentials": "true",
-        },
-      });
-    }
-
-    const textResponse = await response.text();
-    return new Response(textResponse, {
-      status: response.status,
-      statusText: response.statusText,
-      headers: {
-        "Content-Type": contentType,
-        "Access-Control-Allow-Origin": "https://spiral-web-client.pages.dev",
-        "Access-Control-Allow-Credentials": "true",
-      },
-    });
-  } catch (error) {
-    console.error("Error:", error);
-    return new Response("Internal Server Error", { status: 500 });
-  }
+  return response;
 }
